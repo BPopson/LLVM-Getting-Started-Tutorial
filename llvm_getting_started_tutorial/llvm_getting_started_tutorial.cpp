124711973c6f62211b04bb326121a196783c7f9a
Brandon Popson <brandon.popson@gmail.com>
2024-11-11 11:25:36 -0500

    Adjusts formatting

--- llvm_getting_started_tutorial/llvm_getting_started_tutorial.cpp
+++ llvm_getting_started_tutorial/llvm_getting_started_tutorial.cpp
@@ -115,7 +115,8 @@ class BinaryExprAST : public ExprAST {
 	std::unique_ptr<ExprAST> LHS, RHS;
 
 public:
-	BinaryExprAST(char Op, std::unique_ptr<ExprAST> LHS, std::unique_ptr<ExprAST> RHS)
+	BinaryExprAST(char Op, std::unique_ptr<ExprAST> LHS,
+				  std::unique_ptr<ExprAST> RHS)
 		: Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
 };
 
