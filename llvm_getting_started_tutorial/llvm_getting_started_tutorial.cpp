9c0c2cb42c67cefd4a0179a2fc58e3601abb5520
Brandon Popson <brandon.popson@gmail.com>
2024-11-11 11:22:37 -0500

    Adds BinaryExprAST class

--- llvm_getting_started_tutorial/llvm_getting_started_tutorial.cpp
+++ llvm_getting_started_tutorial/llvm_getting_started_tutorial.cpp
@@ -109,6 +109,15 @@ public:
 	VariableExprAST(const std::string& Name) : Name(Name) {}
 };
 
+/// BinaryExprAST - Expression class for a binary operator.
+class BinaryExprAST : public ExprAST {
+	char Op;
+	std::unique_ptr<ExprAST> LHS, RHS;
+
+public:
+	BinaryExprAST(char Op, std::unique_ptr<ExprAST> LHS, std::unique_ptr<ExprAST> RHS)
+		: Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+};
 
 
 int main() {
