9a43516c1642fbd0d89e5048f1bdacacedd1b451
Brandon Popson <brandon.popson@gmail.com>
2024-11-11 11:06:33 -0500

    Changes comment to documentation comment

--- llvm_getting_started_tutorial/llvm_getting_started_tutorial.cpp
+++ llvm_getting_started_tutorial/llvm_getting_started_tutorial.cpp
@@ -25,7 +25,7 @@ enum Token {
 static std::string IdentifierStr;	// Filled in if tok_identifier
 static double NumVal;				// Filled in if tok_number
 
-// gettok - Return the next token from standard input.
+/// gettok - Return the next token from standard input.
 static int gettok() {
 	static int LastChar = ' ';
 
