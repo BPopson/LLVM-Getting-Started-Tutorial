5060a6c220b1c5890368683d983aeffad32658db
Brandon Popson <brandon.popson@gmail.com>
2024-11-11 11:32:06 -0500

    Adds PrototypeAST class definition

--- llvm_getting_started_tutorial/llvm_getting_started_tutorial.cpp
+++ llvm_getting_started_tutorial/llvm_getting_started_tutorial.cpp
@@ -131,6 +131,19 @@ public:
 		: Callee(Callee), Args(std::move(Args)) {}
 };
 
+/// PrototypeAST - This class represents the "prototype" for a function,
+/// which captures its name, and its argument names (this implicitly the number
+/// of arguments the functino takes).
+class PrototypeAST {
+	std::string Name;
+	std::vector<std::string> Args;
+
+public:
+	PrototypeAST(const std::string& Name, std::vector<std::string> Args)
+		: Name(Name), Args(std::move(Args)) {}
+	
+	const std::string& getName() const { return Name; }
+};
 
 int main() {
 	return 0;
