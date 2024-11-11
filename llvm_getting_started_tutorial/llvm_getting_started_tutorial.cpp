8706a0db1e6f599e0b276549fe0c4df91c41bb5c
Brandon Popson <brandon.popson@gmail.com>
2024-11-11 15:07:45 -0500

    Adds additional parsing, and main driver code

--- llvm_getting_started_tutorial/llvm_getting_started_tutorial.cpp
+++ llvm_getting_started_tutorial/llvm_getting_started_tutorial.cpp
@@ -410,6 +410,67 @@ static std::unique_ptr<FunctionAST> ParseTopLevelExpr() {
 	return nullptr;
 }
 
+//===----------------------------------------------------------------------===//
+// Top-Level parsing
+//===----------------------------------------------------------------------===//
+
+static void HandleDefinition() {
+	if (ParseDefinition()) {
+		fprintf(stderr, "Parsed a function definition.\n");
+	}
+	else {
+		// Skip token for error recovery.
+		getNextToken();
+	}
+}
+
+static void HandleExtern() {
+	if (ParseExtern()) {
+		fprintf(stderr, "Parsed an extern\n");
+	}
+	else {
+		// Skip token for error recovery.
+		getNextToken();
+	}
+}
+
+static void HandleTopLevelExpression() {
+	// Evaluate a top-level expression into an anonymous function.
+	if (ParseTopLevelExpr()) {
+		fprintf(stderr, "Parsed a top-level expr\n");
+	}
+	else {
+		// Skip token for error recovery.
+		getNextToken();
+	}
+}
+
+/// top ::= definition | external | expression | ';'
+static void MainLoop() {
+	while (true) {
+		fprintf(stderr, "ready> ");
+		switch (CurTok) {
+		case tok_eof:
+			return;
+		case ';': // Ignore top-level semicolons
+			getNextToken();
+			break;
+		case tok_def:
+			HandleDefinition();
+			break;
+		case tok_extern:
+			HandleExtern();
+			break;
+		default:
+			HandleTopLevelExpression();
+			break;
+		}
+	}
+}
+
+//===----------------------------------------------------------------------===//
+// Main driver code.
+//===----------------------------------------------------------------------===//
 
 int main() {
 	// Install standard binary operators
@@ -418,6 +479,13 @@ int main() {
 	BinopPrecedence['+'] = 20;
 	BinopPrecedence['-'] = 20;
 	BinopPrecedence['*'] = 40; // Highest
+
+	// Prime the first token
+	fprintf(stderr, "ready> ");
+	getNextToken();
+
+	// Run the main "interpreter loop" now
+	MainLoop();
 
 	return 0;
 }
