aabbff14fb722097e4fb2233ef41c187a36cc1f9
Brandon Popson <brandon.popson@gmail.com>
2024-11-11 11:34:22 -0500

    Adds FunctionAST class definition

--- llvm_getting_started_tutorial/llvm_getting_started_tutorial.cpp
+++ llvm_getting_started_tutorial/llvm_getting_started_tutorial.cpp
@@ -145,6 +145,17 @@ public:
 	const std::string& getName() const { return Name; }
 };
 
+/// FunctionAST - This class represents a function definition itself.
+class FunctionAST {
+	std::unique_ptr<PrototypeAST> Proto;
+	std::unique_ptr<ExprAST> Body;
+
+public:
+	FunctionAST(std::unique_ptr<PrototypeAST> Proto,
+				std::unique_ptr<ExprAST> Body)
+		: Proto(std::move(Proto)), Body(std::move(Body)) {}
+};
+
 int main() {
 	return 0;
 }
