780831d72616addd88b5f292a5571f1ab6e62d62
Brandon Popson <brandon.popson@gmail.com>
2024-11-11 11:07:03 -0500

    Starts adding abstract syntax tree classes

--- llvm_getting_started_tutorial/llvm_getting_started_tutorial.cpp
+++ llvm_getting_started_tutorial/llvm_getting_started_tutorial.cpp
@@ -84,4 +84,18 @@ static int gettok() {
 	return ThisChar;
 }
 
+// ExprAST - Base class for all expression nodes.
+class ExprAST {
+public:
+	virtual ~ExprAST() = default;
+};
+
+/// NumberExprAST - Expression class for number literals like "1.0".
+class NumberExprAST : public ExprAST {
+	double Val;
+
+public:
+	NumberExprAST(double Val) : Val(Val) {}
+};
+
 
