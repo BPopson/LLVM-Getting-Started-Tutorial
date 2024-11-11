fbf46052c196d86542219bd241c8d0e7b2fb58c6
Brandon Popson <brandon.popson@gmail.com>
2024-11-11 13:13:13 -0500

    Starts adding support for Binary Precedence

--- llvm_getting_started_tutorial/llvm_getting_started_tutorial.cpp
+++ llvm_getting_started_tutorial/llvm_getting_started_tutorial.cpp
@@ -266,6 +266,32 @@ static std::unique_ptr<ExprAST> ParsePrimary() {
 	}
 }
 
+/// BinopPrecedence - This holds the precendence for each binary operator that
+/// is defined.
+static std::map<char, int> BinopPrecedence;
+
+/// GetTokPrecedence - Get the precedence of hte pending binary operator token.
+static int GetTokPrecedence() {
+	if (!isascii(CurTok)) {
+		return -1;
+	}
+
+	// Make sure it's a declared binop
+	int TokPrec = BinopPrecedence[CurTok];
+	if (TokPrec <= 0) {
+		return -1;
+	}
+
+	return TokPrec;
+}
+
 int main() {
+	// Install standard binary operators
+	// 1 is lowest precedence
+	BinopPrecedence['<'] = 10;
+	BinopPrecedence['+'] = 20;
+	BinopPrecedence['-'] = 20;
+	BinopPrecedence['*'] = 40; // Highest
+
 	return 0;
 }
